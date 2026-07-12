16766427575b7d980b766b77e44b7a8c9e95d308
Chaudhary Anil <chaudharyanil5656@gmail.com>
2026-07-12 08:08:35 +0545

    user error response discussion

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import connectDB from "./db/index.js";
 import { configDotenv } from "dotenv";
 import { DB_NAME } from "./constants.js";
+import { app } from "./app.js";
 
 
 configDotenv()
 
+
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running on port: ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log(`Mongo db connection error: ${error}`)
+}) 
 
 
 
---
+++ src/utills/asyncHandler.js
@@ -0,0 +1,25 @@
+import connectDB from "../db"
+
+// route.post("/", asyncHandler(createNote))
+
+
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error))
+    }
+}
+
+
+export { asyncHandler }
+
+
+// const asyncHandler = (fn) => async (req, res, next) => { 
+//     try{
+//         await fn(req, res, next)
+//     } catch(error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// } 
